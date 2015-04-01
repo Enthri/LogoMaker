344e8225d19ab5af247db1e42bfd2e97b4d18a1d
Troi-Ryan Stoeffler <troiryan2@gmail.com>
2015-03-31 20:34:35 -0700

    finished layers

--- src/Engine.java
+++ src/Engine.java
@@ -7,12 +7,17 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -65,8 +70,30 @@ public class Engine {
 		layerContainerParent.setBorder(BorderFactory.createTitledBorder("Layers"));
 		frame.add(layerContainerParent, BorderLayout.SOUTH);
 		toolPanel = new JPanel();
-		toolPanel.setPreferredSize(new Dimension(110, 0));
-		toolPanel.setBorder(BorderFactory.createTitledBorder("Tools"));
+		toolPanel.setLayout(new BoxLayout(toolPanel, BoxLayout.Y_AXIS));
+		toolPanel.setBorder(null);
+		toolPanel.addComponentListener(new ComponentListener() {
+			@Override
+			public void componentHidden(ComponentEvent e) {
+			}
+
+			@Override
+			public void componentMoved(ComponentEvent e) {
+			}
+
+			@Override
+			public void componentResized(ComponentEvent e) {
+				if(toolPanel.getSize().getWidth() >= 5 && toolPanel.getBorder() == null) {
+					toolPanel.setBorder(BorderFactory.createTitledBorder("Tools"));
+				} else if(toolPanel.getSize().getWidth() < 5 && toolPanel.getBorder() != null) {
+					toolPanel.setBorder(null);
+				}
+			}
+
+			@Override
+			public void componentShown(ComponentEvent e) {
+			}
+		});
 		frame.add(toolPanel, BorderLayout.EAST);
 		JMenu temp;
 		JMenuBar menubar = new JMenuBar();
@@ -108,11 +135,25 @@ public class Engine {
 		temp.add(createMenuItem("Add Layer", new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				addLayer("Layer: " + layerList.size());
+				addLayer(new Layer(painter, "Layer: " + layerList.size()));
 			}
 		}));
 		menubar.add(temp);
 		frame.setJMenuBar(menubar);
+		frame.addKeyListener(new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(layerList.size() > 0) layerList.get(0).keyPressed(e.getKeyCode());
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+			}
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+		});
 		frame.setVisible(true);
 	}
 	
@@ -127,9 +168,28 @@ public class Engine {
 		c.repaint();
 	}
 	
-	private static void addLayer(String name) {
-		layerList.add(new Layer(name));
-		layerContainer.add(layerList.get(layerList.size() - 1).getButton());
+	private static void addLayer(Layer layer) {
+		layerList.add(layer);
+		layerContainer.add(layer.getButton());
+		layer.getButton().addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				selectLayer(layer);
+			}
+		});
 		revaluate(layerContainer);
+		selectLayer(layer);
+	}
+	
+	private static void selectLayer(Layer layer) {
+		if(layerList.indexOf(layer) > -1) {
+			for(Layer layer2 : layerList) layer2.getButton().setEnabled(true);
+			layer.getButton().setEnabled(false);
+			layerList.remove(layerList.indexOf(layer));
+			layerList.add(0, layer);
+			toolPanel.removeAll();
+			layer.drawUI(toolPanel);
+			revaluate(toolPanel);
+		}
 	}
 }
--- src/Layer.java
+++ src/Layer.java
@@ -1,14 +1,19 @@
 import java.awt.Graphics2D;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
 
 public class Layer {
 	
 	private String name;
 	private JButton button;
 	
-	public Layer(String stringVar) {
+	private JComponent painter;
+	
+	public Layer(JComponent jcomponentVar, String stringVar) {
 		name = stringVar;
+		painter = jcomponentVar;
 		button = new JButton(stringVar);
 	}
 	
@@ -21,6 +26,11 @@ public class Layer {
 	}
 	
 	public void paint(Graphics2D render) {
-		
+	}
+	
+	public void drawUI(JPanel component) {
+	}
+	
+	public void keyPressed(int keyCode) {
 	}
 }
